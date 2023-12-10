dcc079d282e374ff778b5b35101ae1370ffe40e4
Jacques Gagnon <darthcloud@gmail.com>
2023-12-10 14:00:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ESP32-BT-Dongle
+
+Host drivers and target firmware for using an ESP32 as a Bluetooth dongle.
---
+++ main/main.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023, Jacques Gagnon
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "sdkconfig.h"
+
+void app_main()
+{
+    printf("ESP32-BT-Dongle\n");
+}
